e93fd1891917131ed8ea3d7d870683ba82ae9798
perlancar (on pc-home) <perlancar@gmail.com>
2020-03-01 11:21:12 +0700

    Initial commit

---
+++ lib/Acme/MetaSyntactic/gavan.pm
@@ -0,0 +1,48 @@
+package Acme::MetaSyntactic::gavan;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use parent qw(Acme::MetaSyntactic::MultiList);
+__PACKAGE__->init;
+
+1;
+# ABSTRACT: The Space Sheriff Gavan theme
+
+=head1 SYNOPSIS
+
+ % perl -MAcme::MetaSyntactic=gavan -le 'print metaname'
+ mimi
+
+ % metasyn gavan | shuf | head -n2
+ qom
+ jaaku
+
+
+=head1 SEE ALSO
+
+L<https://en.wikipedia.org/wiki/Space_Sheriff_Gavan>
+
+L<Acme::MetaSyntactic::metal_heroes>
+
+L<Acme::MetaSyntactic>
+
+=cut
+
+__DATA__
+# default
+:all
+# names good
+gavan mimi qom marin voicer alan deniga tsukiko hoshino kojiro oyama gosuke
+fuji wakaba yoichi shigeru touyama
+# names evil
+donhorror horrorgirl hunterkiller sandorva kiba doubleman crusher
+# names monster
+shako gamara condor sasori dokuja oomadako ari kaenzeru nijichou armadillo goat
+sai shamo kama hyou aogame kyouryuu kera mitsubachi kerage kumo saber goshiki
+gas jaaku hakkotsu magic keibi saimin totsugeki kaibutsu doctor guts urami
+anahori gang nottori yokai jigoku buffalo lizard rhino
+# names weapon
+laserblade laserzbeam barrier laserscope electrosonar cyberian gavion scooper
